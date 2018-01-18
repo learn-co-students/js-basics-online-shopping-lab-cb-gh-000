c61402c36060acd633c4012dce4461cf1740f0ed
edwin rivera <waehake@gmail.com>
2018-01-18 03:14:31 +0000

    Done.

--- index.js
+++ index.js
@@ -11,20 +11,68 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var cartObject = {[item]: Math.floor((Math.random() * 100) + 1) };
+ cart.push(cartObject);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  if (!cart.length) {
+    return console.log('Your shopping cart is empty.');
+  }
+
+  const itemsWithPrices = [];
+
+  for (let i = 0; i < cart.length; i++) {
+    var itemNames = Object.keys(cart[i]);
+    var itemName = itemNames[0];
+    var itemPrice = cart[i][itemName]
+
+    itemsWithPrices.push(`${itemName} at $${itemPrice}`)
+  }
+
+
+  if (itemsWithPrices.length === 1) {
+    return console.log(`In your cart, you have ${itemsWithPrices.join()}.`)
+  } else if (itemsWithPrices.length === 2) {
+    return console.log(`In your cart, you have ${itemsWithPrices[0]} and ${itemsWithPrices.slice(-1)}.`)
+  } else {
+    return console.log(`In your cart, you have ${itemsWithPrices.slice(0,-1).join(', ')}, and ${itemsWithPrices.slice(-1)}.`)
+  }
 }
 
 function total() {
   // write your code here
+  var totalcost = 0;
+  for (var i = 0; i < cart.length; i++) {
+    var itemNames = Object.keys(cart[i]);
+    var itemName = itemNames[0];
+    totalcost += parseInt(cart[i][itemName], 10);
+  }
+
+  return totalcost;
 }
 
 function removeFromCart(item) {
   // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      return cart.splice(i, 1);
+    }
+  }
+
+  return console.log('That item is not in your cart.');
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+    return cart;
+  } else {
+    return console.log("Sorry, we don\'t have a credit card on file for you.");
+  }
 }
