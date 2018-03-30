8eb2eedd30d2ab9c2c63f80e72ac2c374ce22a39
norman lewis <takialog@gmail.com>
2018-03-30 04:35:28 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,52 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push({ itemName:`${item}`, itemPrice: Math.floor(Math.random() * 100)})
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  function generateCartDescription() {
+    var desc = 'In your cart,'
+    if (getCart().length >= 1) {
+      desc +=  ` you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
+    }
+    if (getCart().length >= 2) {
+      var middleDesc = ''
+      for (let i = 1; i < getCart().length - 1; i++) {
+        middleDesc += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+      }
+      desc += middleDesc + `, and ${getCart()[getCart().length - 1].itemName} at $${getCart()[getCart().length - 1].itemPrice}`
+    }
+    return `${desc}.`
+  }
+   return getCart().length === 0 ? 'Your shopping cart is empty.' : generateCartDescription()
 }
 
 function total() {
-  // write your code here
+  var sum = 0
+  for (let i=0; i<getCart().length; i++ ) {
+     sum += getCart()[i].itemPrice
+  }
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var indexOfItem
+  for (let i=0; i < getCart().length; i++) {
+    if (getCart()[i].itemName == item) {
+      indexOfItem = i
+    }
+  }
+  return (indexOfItem) ? getCart().splice(indexOfItem, 1) : 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    getCart().length = 0
+    var sumToCharge = total()
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  } else {
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
