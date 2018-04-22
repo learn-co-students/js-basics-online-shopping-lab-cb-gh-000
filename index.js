4e009da1dc33e3d41a513c87886fb1ef6a15617a
Abdelrauof <34385173+abdelrauof97@users.noreply.github.com>
2018-04-22 23:39:06 +0200

    added some basis

--- index.js
+++ index.js
@@ -10,11 +10,29 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({itemName: item, itemPrice: Math.floor(Math.random() * 100)})
+  return item + ' has been added to your cart.'
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.'
+  }
+  else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0]['itemName']} at ${cart[0]['itemPrice']}.`
+  }
+  else {
+    var new_array = cart
+    for (var i = 0; i < cart.length; i++) {
+      if (i === (cart.length - 1)) {
+        new_array(`and ${cart[i]['itemName']} at ${cart[i]['itemPrice']}`)
+      }
+      else {
+        new_array.push(`${cart[i]['itemName']} at ${cart[i]['itemPrice']}`)
+      }
+    }
+    return `In your cart, you have ${new_array.join(", ")}.`
+  }
 }
 
 function total() {
