f770b8e81e9a5947c2e63ca95ae69835b1599131
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-19 10:49:43 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+var item_object = {"itemName" : item, "itemPrice" : Math.floor(Math.random(1,100))}
+cart.push(item_object)
+return "${item} has been added to your cart."
 }
 
 function viewCart() {
